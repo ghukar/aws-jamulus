87919dde1d1df5be8e1e684c09faec0f1df2f7f2
Carsten Koch <carsten.b.koch@gmail.com>
2021-05-08 15:05:40 +0200

    fix: added removal policy for S3 bucket

--- lib/create-config-bucket.ts
+++ lib/create-config-bucket.ts
@@ -1,9 +1,12 @@
 import { Bucket } from "@aws-cdk/aws-s3";
 import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
-import { Stack } from "@aws-cdk/core";
+import { RemovalPolicy, Stack } from "@aws-cdk/core";
 
 export const createConfigBucket = (scope: Stack) => {
-  const bucket = new Bucket(scope, 'JamulusConfigBucket', { bucketName: 'jamulus-config-bucket' });
+  const bucket = new Bucket(scope, 'JamulusConfigBucket', {
+    bucketName: 'jamulus-config-bucket',
+    removalPolicy: RemovalPolicy.DESTROY,
+  });
   new BucketDeployment(scope, 'BucketDeployment', {
     destinationBucket: bucket,
     sources: [Source.asset('./server-config')],
